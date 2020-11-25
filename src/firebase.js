fdad5d0f1b90fa661ed01b93e7b73ae2b0b185cc
Aashish Sunuwar <69027786+spydermyaan@users.noreply.github.com>
2020-11-25 10:45:10 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBDEg3RIQAr6DihDbE0jj3H2chZZ_nyJMM",
-  authDomain: "instagram-clone-60064.firebaseapp.com",
-  databaseURL: "https://instagram-clone-60064.firebaseio.com",
-  projectId: "instagram-clone-60064",
-  storageBucket: "instagram-clone-60064.appspot.com",
-  messagingSenderId: "827238987257",
-  appId: "1:827238987257:web:705e7733826aa0610d7928",
-  measurementId: "G-724LPH5R4F",
+    apiKey: "AIzaSyCVfoiFhBM8SshCZDmjzrG7yeJLDko5BZk",
+    authDomain: "ig-clone-4b4b2.firebaseapp.com",
+    databaseURL: "https://ig-clone-4b4b2.firebaseio.com",
+    projectId: "ig-clone-4b4b2",
+    storageBucket: "ig-clone-4b4b2.appspot.com",
+    messagingSenderId: "627126945724",
+    appId: "1:627126945724:web:faed3ffe32222510530db9",
+    measurementId: "G-Z0VD6SW08X"
 });
 
 const db = firebaseApp.firestore();
